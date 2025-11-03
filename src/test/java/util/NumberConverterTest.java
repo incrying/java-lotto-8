a764fa37f61b126322661d9789a98333bdeeec69
인다운 <incrying@duksung.ac.kr>
2025-11-03 22:54:29 +0900

    feat(NumberConverter): 문자열에서 정수로 변환 기능 작성

---
+++ src/test/java/util/NumberConverterTest.java
@@ -0,0 +1,52 @@
+package util;
+
+import lotto.util.NumberConverter;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class NumberConverterTest {
+    @DisplayName("문자열 리스트가 숫자 리스트로 변환된다.")
+    @Test
+    void convertToIntegerList() {
+        List<String> strings = List.of("1","2","3","4","5","6");
+
+        List<Integer> integers = NumberConverter.convertToIntegerList(strings);
+
+        assertThat(integers).isEqualTo(List.of(1,2,3,4,5,6));
+    }
+
+    @DisplayName("공백을 제거한 문자열 리스트를 숫자 리스트로 변환한다.")
+    @Test
+    void convertToIntegerList_hasSpaces() {
+        List<String> strings = List.of("1 "," 2","  3","4","5","6");
+
+        List<Integer> integers = NumberConverter.convertToIntegerList(strings);
+
+        assertThat(integers).isEqualTo(List.of(1,2,3,4,5,6));
+    }
+
+    @DisplayName("문자열을 숫자로 변환한다.")
+    @Test
+    void convertToInteger() {
+        String input = "1";
+
+        Integer converted = NumberConverter.convertToInteger(input);
+
+        assertThat(converted).isEqualTo(1);
+    }
+
+    @DisplayName("공백을 제거하고 문자열이 숫자로 변환된다.")
+    @Test
+    void convertToInteger_hasSpaces() {
+
+        String input = "  1   ";
+
+        Integer converted = NumberConverter.convertToInteger(input);
+
+        assertThat(converted).isEqualTo(1);
+    }
+}
